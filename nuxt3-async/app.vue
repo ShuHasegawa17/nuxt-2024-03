9f3610ac06d1200fb4e51b85cd25fa412db96e4a
shuhasegawa <shuhasegawa0430@gmail.com>
2024-03-12 10:58:21 +0900

    async

---
+++ nuxt3-async/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { data: users, error } = await useFetch<any>(
+  'https://jsonplaceholder.typicode.com/users2'
+);
+console.log(error.value);
+if (error.value) {
+  throw createError({ statusCode: 404, statusMessage: 'page not found!!' });
+}
+</script>
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <!-- <p>{{ users[0].id }} {{ users[0].name }}</p> -->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }} {{ user.name }} : {{ user.phone }}
+      </li>
+    </ul>
+  </div>
+</template>
